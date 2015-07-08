bbf0f655969321149c8a68d9fc56385022ec6e2b
曹溦 <caowei0218@qq.com>
2015-07-08 15:12:47 +0800

    lib包

---
+++ src/com/example/spreadapp/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.spreadapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Æô¶¯Activity
+ * */
+public class SplashActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
