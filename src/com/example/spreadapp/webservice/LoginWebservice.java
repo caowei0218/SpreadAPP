6492b79355a0e3c93ed97277758d4b473fef0997
曹溦 <caowei0218@qq.com>
2015-07-17 15:17:17 +0800

    登陆接口搞定

--- src/com/example/spreadapp/webservice/LoginWebservice.java
+++ src/com/example/spreadapp/webservice/LoginWebservice.java
@@ -6,7 +6,6 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
@@ -14,11 +13,8 @@ import android.widget.Toast;
 
 import com.example.spreadapp.activity.LoginActivity;
 import com.example.spreadapp.bean.UserBean;
-import com.example.spreadapp.util.Common;
-import com.example.spreadapp.util.JsonBinder;
 
 public class LoginWebservice extends AsyncTask<String, Integer, String> {
-	private JsonBinder jsonBinder = JsonBinder.buildNonDefaultBinder();
 	public final String METHODNAME = "login";
 	private HttpTransportSE ht = null;
 	private SoapSerializationEnvelope envelope;
@@ -43,44 +39,31 @@ public class LoginWebservice extends AsyncTask<String, Integer, String> {
 		envelope = new SoapSerializationEnvelope(SoapEnvelope.VER10);
 		send = new SoapObject(WebserviceUtils.NAMESPACE, METHODNAME);
 		envelope.bodyOut = send;
+
 		String pwd = userBean.getPassword();
 		String account = userBean.getAccount();
-		send.addProperty("account", account);
-		send.addProperty("pwd", pwd);
-		envelope.addMapping(WebserviceUtils.NAMESPACE, "UserBean",
-				UserBean.class);
+		send.addProperty("userName", account);
+		send.addProperty("password", pwd);
+
 		envelope.dotNet = false;
 	}
 
 	protected String doInBackground(String... params) {
-		String resultString = null;
+		String result = null;
 		try {
 			ht.call(WebserviceUtils.NAMESPACE + "/" + METHODNAME, envelope);
-		} catch (Exception e) {
-			System.out.println(e.getMessage() + "连接失败");
-			e.printStackTrace();
-			resultString = "连接失败，请检查网络连接";
-			return resultString;
-		}
-		try {
 			Object soapObject = (Object) envelope.getResponse();
-			if (soapObject != null) {
-				userBean = jsonBinder.fromJson(soapObject.toString(),
-						UserBean.class);
-				if (userBean == null) {
-					resultString = "用户名或密码错误";
-				} else {
-					resultString = "登陆成功";
-					Common.userCommon = userBean;
-				}
+			if ("ok".equals(soapObject.toString())) {
+				result = "登陆成功";
 			} else {
-				resultString = "soapObject为空";
+				result = "用户名密码错误";
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			resultString = "账号不存在";
+			result = "连接失败，请检查网络连接";
+			return result;
 		}
-		return resultString;
+		return result;
 	}
 
 	protected void onPostExecute(String result) {
@@ -90,9 +73,9 @@ public class LoginWebservice extends AsyncTask<String, Integer, String> {
 		if ("登陆成功".equals(result)) {
 			saveLoginUserName(userBean.getAccount(), userBean.getPassword(),
 					userBean.getUser_id());
-			Intent intent = new Intent(context, LoginActivity.class);
-			context.startActivity(intent);
-			loginActivity.finish();
+//			Intent intent = new Intent(context, LoginActivity.class);
+//			context.startActivity(intent);
+//			loginActivity.finish();
 		}
 	}
 
--- src/com/example/spreadapp/webservice/WebserviceUtils.java
+++ src/com/example/spreadapp/webservice/WebserviceUtils.java
@@ -2,12 +2,8 @@ package com.example.spreadapp.webservice;
 
 public class WebserviceUtils {
 
-	public static final String IP = "http://10.86.255.26";
-	public static final String PORT = "8080";
-	public static final String NAMESPACE = "http://service.soa.ecoo.app.eoe";
-	public static final String SOURCE_FILE = "/cxcweb/services/Servicecxc";
-
-	public static final String HTTPTRANSPORTSE = "http://117.78.20.206:8080/ecooSoa/services/ExcooWebserviceImpl";
-	// public static final String HTTPTRANSPORTSE =
-	// "http://10.86.255.22:8080/ecooSoa/services/ExcooWebserviceImpl";
+//	public static final String NAMESPACE = "http://service.soa.ecoo.app.eoe";
+	public static final String NAMESPACE = "http://service.example.com";
+	
+	public static final String HTTPTRANSPORTSE = "http://10.86.255.26:8080/SpreadWeb/services/ServiceAPP/";
 }
